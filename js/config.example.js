9c888cd67d4e7008ccb044412734e6202176ac5c
Gabriella Rodrigues <rlsgabriella@gmail.com>
2026-09-21 12:01:18 -0300

    chore: remove credenciais do tracking e cria arquivo de configuracao exemplo

---
+++ js/config.example.js
@@ -0,0 +1,10 @@
+window._config = {
+  cognito: {
+    userPoolId: "YOUR_USER_POOL_ID",
+    userPoolClientId: "YOUR_USER_POOL_CLIENT_ID",
+    region: "us-east-1",
+  },
+  api: {
+    invokeUrl: "YOUR_API_GATEWAY_URL",
+  },
+};
